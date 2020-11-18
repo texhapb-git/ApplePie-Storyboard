2f1d5b2ed6f250ecb00a46e35a335087d0e7951a
TexHaPb <texhapb@bk.ru>
2020-11-18 19:08:26 +0300

    Initial Commit

---
+++ ApplePie Storyboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ApplePie Storyboard
+//
+//  Created by Максим Иванов on 18.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
