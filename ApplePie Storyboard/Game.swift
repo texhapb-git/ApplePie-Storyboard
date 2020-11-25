7f1a2a66a2837b2477a61447351544bdcc842fff
TexHaPb <texhapb@bk.ru>
2020-11-25 21:10:51 +0300

    Added GameModel

---
+++ ApplePie Storyboard/Game.swift
@@ -0,0 +1,11 @@
+//
+//  Game.swift
+//  ApplePie Storyboard
+//
+//  Created by Максим Иванов on 25.11.2020.
+//
+
+struct Game {
+    var guessWord: String
+    var incorrectMovesRemaining: Int
+}
--- ApplePie Storyboard/ViewController.swift
+++ ApplePie Storyboard/ViewController.swift
@@ -9,17 +9,122 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - IB Outlets
     @IBOutlet weak var treeImageView: UIImageView!
-    
     @IBOutlet var letterButtons: [UIButton]!
-    
     @IBOutlet weak var guessWordLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
     
+    // MARK: -Properties
+    var currentGame: Game!
+    let incorrectMovesAllowed = 7
+    var listOfCities = [
+        "Александрия",
+        "Атланта",
+        "Ахмедабад",
+        "Багдад",
+        "Бангалор",
+        "Бангкок",
+        "Барселона",
+        "Белу-Оризонти",
+        "Богота",
+        "Буэнос-Айрес",
+        "Вашингтон",
+        "Гвадалахара",
+        "Гонконг",
+        "Гуанчжоу",
+        "Дакка",
+        "Даллас",
+        "Далянь",
+        "Дар-эс-Салам",
+        "Дели",
+        "Джакарта",
+        "Дунгуань",
+        "Йоханнесбург",
+        "Каир",
+        "Калькутта",
+        "Карачи",
+        "Киншаса",
+        "Куала Лумпур",
+        "Лагос",
+        "Лахор",
+        "Лима",
+        "Лондон",
+        "Лос-Анджелес",
+        "Луанда",
+        "Мадрид",
+        "Майами",
+        "Манила",
+        "Мехико",
+        "Москва",
+        "Мумбаи",
+        "Нагоя",
+        "Нанкин",
+        "Нью-Йорк",
+        "Осака",
+        "Париж",
+        "Пекин",
+        "Пуна",
+        "Рио-де-Жанейро",
+        "Сан-Паулу",
+        "Санкт-Петербург",
+        "Сантьяго",
+        "Сеул",
+        "Сиань",
+        "Сингапур",
+        "Стамбул",
+        "Сурат",
+        "Сучжоу",
+        "Тегеран",
+        "Токио",
+        "Торонто",
+        "Тяньцзинь",
+        "Ухань",
+        "Филадельфия",
+        "Фошань",
+        "Фукуока",
+        "Хайдарабад",
+        "Ханчжоу",
+        "Харбин",
+        "Хартум",
+        "Хошимин",
+        "Хьюстон",
+        "Цзинань",
+        "Циндао",
+        "Ченнай",
+        "Чикаго",
+        "Чунцин",
+        "Чэнду",
+        "Шанхай",
+        "Шэньчжэнь",
+        "Шэньян",
+        "Эр-Рияд",
+        "Янгон",
+    ].shuffled()
+    
+    var totalWins = 0
+    var totalLosts = 0
+    
+    // MARK: - Methods
+    func newRound() {
+        let newCity = listOfCities.removeFirst()
+        currentGame = Game(guessWord: newCity, incorrectMovesRemaining: incorrectMovesAllowed)
+        updateUI()
+    }
+    
+    func updateUI() {
+        let movesRemaining = currentGame.incorrectMovesRemaining
+        let imageName = "Tree\(movesRemaining < 8 ? movesRemaining : incorrectMovesAllowed)"
+        treeImageView.image = UIImage(named: imageName)
+        scoreLabel.text = "Выигрыши: \(totalWins)   Проигрыши: \(totalLosts)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        newRound()
     }
+    
+    
     
     // MARK: - IB Actions
     
